aee52002bf570ce3f58a8ca708ba7b55dd2db5ca
Hygel1 <75685854+Hygel1@users.noreply.github.com>
2025-11-05 01:36:29 -0500

    up

--- FinalProjTestFiles/getPWM.c
+++ FinalProjTestFiles/getPWM.c
@@ -11,29 +11,31 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
-
+uint32_t *on[6] = malloc(24) //6*32/8=24 bytes to be allocated, won't be deallocated until manually done
+//malloc to ensure it doesn't dereference; outside of method so we update instead of recreate the same storage on every run
+//calling this function will take just over 20kus=.02s, this should be taken into account when used
 uint32_t* get_duty(uint32_t pin[]) {
     uint32_t t1, t2, t3, t4;
-    const uint32_t period_us = 20000; //full PWM cycle
+    const uint32_t period_us = 20000; //full PWM cycle, takes final, wrapped average - doesn't really matter where in the duty cycle it starts
     const uint32_t readPeriod = 10; //time between sampling is .01ms
-    static uint32_t on[6];
     t1 = t3 = t4 = timer_read();
     while (true) {
         t2 = t4 = timer_read();
         if(timer_elapsed_us(t1, t2) >= readPeriod) {
+            t1 = t2 = timer_read(); //reset count, log time before runtime
             if(gpio_get(pin[0]) == 1) on[0]++;
             if(gpio_get(pin[1]) == 1) on[1]++;
             if(gpio_get(pin[2]) == 1) on[2]++;
             if(gpio_get(pin[3]) == 1) on[3]++;
             if(gpio_get(pin[4]) == 1) on[4]++;
             if(gpio_get(pin[5]) == 1) on[5]++;
-            t1 = t2 = timer_read(); //reset count
         }
         if (timer_elapsed_us(t3, t4) >= period_us) {
             printf("On = %d \n", on);
             //percent = on-100;
             for(int i=0;i<6;i++) on[i]=on[i]-100; //returns 100-200 value, subtract 100 to normalize to percent value
             return on; //returns a number array of values 0-100 
+            //need to ensure all of these values are normalized to 0-100 values
             }
     }
 }
--- FinalProjTestFiles/main.c
+++ FinalProjTestFiles/main.c
@@ -12,25 +12,15 @@
 #define UART_RX_PIN 1
 
 
-uint16_t input16;
-uint16_t input17;
-uint16_t input18;
-uint16_t input19;
-uint16_t input20;
-uint16_t input21;
-uint16_t input22;
-
-
-
 //currently the default pwm pin is GPIO 14
 int main() {
     stdio_init_all();
     printf("Starting");
     pwm_pin_init(0x8000);
-    uint16_t pin[] = {19, 20, 21, 22, 26, 27};
+    uint16_t inPins[] = {19, 20, 21, 22, 26, 27};
     uint32_t* duties = get_duty(pin);
     while(1) {
-        duties = get_duty(pin);
+        duties = get_duty(inPins);
         //PWM for 0-180 servoes us 50Hz
         // duty19 = duty19*(185)+550;
         // duty19 = (duty19*100/20000)*(0xffff/100);
@@ -44,27 +34,3 @@ int main() {
 
     }
 }
-
-// int main() {
-//     stdio_init_all();
-//     uart_init(UART_ID, BAUD_RATE);
-//     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-//     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-//     while(1) {
-//         input16 = gpio_get(16);
-//         input17 = gpio_get(17);
-//         input18 = gpio_get(18);
-//         input19 = gpio_get(19);
-//         input20 = gpio_get(20);
-//         input21 = gpio_get(21);
-//         input22 = gpio_get(22);
-//         printf("Reading input 16: %d \n", input16);
-//         printf("Reading input 17: %d \n", input17);
-//         printf("Reading input 18: %d \n", input18);
-//         printf("Reading input 19: %d \n", input19);
-//         printf("Reading input 20: %d \n", input20);
-//         printf("Reading input 21: %d \n", input21);
-//         printf("Reading input 22: %d \n", input22);
-        
-//     }
-// }
